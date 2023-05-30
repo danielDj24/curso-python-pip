b184ddfac955e51193efb43236b470105eb81df6
danielDj24 <danieldj242001@gmail.com>
2023-05-29 22:14:57 -0500

    add uvicorn and webserver

--- web-server/main.py
+++ web-server/main.py
@@ -1,6 +1,23 @@
 #modulo donde la esta la funcion que trae la peticion
 import store 
 
+from fastapi import FastAPI #importamos la lib
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+@app.get('/') #ruta para ver desde la web
+def get_list():
+    return [1, 2, 3,]
+
+@app.get('/contact', response_class=HTMLResponse) #ruta
+def get_list():
+    return """
+        <h1> Hola soy una pagina</h1>
+        <p>soy un parrafo</p>
+""" 
+
 def run():
     
 #llamamos la funcion del modulo anterior
