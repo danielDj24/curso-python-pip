7af9375a1e73f464b4555be92a00002fbb0a1e2e
danielDj24 <danieldj242001@gmail.com>
2023-05-29 21:31:50 -0500

    add pandas > APP

--- APP/main.py
+++ APP/main.py
@@ -1,15 +1,30 @@
 import utils
 import read_csv
 import charts
+import pandas as pd # libreria que filtra datos, lee csv y filtrar
+
 
 def run():
-  data = read_csv.read_csv('data.csv')
+  '''
+  
   data = list(filter(lambda item: item['Continent'] == 'South America', data))
   
   countries = list(map(lambda x: x['Country'],data))
   percentages = list(map(lambda x: x['World Population Percentage'],data))
+'''  # llama la funcion que lee el csv de forma manual
+
+  
+  
+  #data frames (datos de donde obtenemos info)
+  df= pd.read_csv('data.csv'); #metodo para leer el csv y ahoora el algoritmo del mod read
+  df= df[ df['Continent']== 'Europe' ] #filtro por continente
+  
+  countries = df['Country'].values #para obtener los valores de la columna
+  percentages= df['World Population Percentage'].values #obtiene los valores
+  
   charts.generate_pie_chart(countries, percentages)
   
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
   print(country)
   
